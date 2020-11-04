a51f7470e2ff3937c94ab6dd670bf9440883bfe3
Joan Valls Plà <drizztdourden21@hotmail.com>
2020-11-04 14:19:28 +0100

    delete photo

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -56,44 +56,61 @@ export function usePhotoGallery() {
         const newPhotos = [savedFileImage, ...photos];
         setPhotos(newPhotos);
         set(PHOTO_STORAGE, JSON.stringify(newPhotos));
-      };
+    };
   
-      const savePicture = async (photo: CameraPhoto, fileName: string): Promise<Photo> => {
+    const savePicture = async (photo: CameraPhoto, fileName: string): Promise<Photo> => {
         let base64Data: string;
         // "hybrid" will detect Cordova or Capacitor;
         if (isPlatform('hybrid')) {
-          const file = await readFile({
+            const file = await readFile({
             path: photo.path!
-          });
-          base64Data = file.data;
+            });
+            base64Data = file.data;
         } else {
-          base64Data = await base64FromPath(photo.webPath!);
+            base64Data = await base64FromPath(photo.webPath!);
         }
         const savedFile = await writeFile({
-          path: fileName,
-          data: base64Data,
-          directory: FilesystemDirectory.Data
+            path: fileName,
+            data: base64Data,
+            directory: FilesystemDirectory.Data
         });
-      
+        
         if (isPlatform('hybrid')) {
-          // Display the new image by rewriting the 'file://' path to HTTP
-          // Details: https://ionicframework.com/docs/building/webview#file-protocol
-          return {
+            // Display the new image by rewriting the 'file://' path to HTTP
+            // Details: https://ionicframework.com/docs/building/webview#file-protocol
+            return {
             filepath: savedFile.uri,
             webviewPath: Capacitor.convertFileSrc(savedFile.uri),
-          };
+            };
         }
         else {
-          // Use webPath to display the new image instead of base64 since it's
-          // already loaded into memory
-          return {
+            // Use webPath to display the new image instead of base64 since it's
+            // already loaded into memory
+            return {
             filepath: fileName,
             webviewPath: photo.webPath
-          };
+            };
         }
-      };
+    };
+
+    const deletePhoto = async (photo: Photo) => {
+      // Remove this photo from the Photos reference data array
+      const newPhotos = photos.filter(p => p.filepath !== photo.filepath);
+    
+      // Update photos array cache by overwriting the existing photo array
+      set(PHOTO_STORAGE, JSON.stringify(newPhotos));
+    
+      // delete photo file from filesystem
+      const filename = photo.filepath.substr(photo.filepath.lastIndexOf('/') + 1);
+      await deleteFile({
+        path: filename,
+        directory: FilesystemDirectory.Data
+      });
+      setPhotos(newPhotos);
+    };
 
     return {
+        deletePhoto,
         photos,
         takePhoto
     };
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -9,7 +9,8 @@ import { usePhotoGallery, Photo } from '../hooks/usePhotoGallery';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
-  const { photos, takePhoto } = usePhotoGallery();
+  const { photos, takePhoto, deletePhoto } = usePhotoGallery();
+  const [photoToDelete, setPhotoToDelete] = useState<Photo>();
   return (
     <IonPage>
       <IonHeader>
@@ -22,16 +23,36 @@ const Tab2: React.FC = () => {
           <IonRow>
             {photos.map((photo, index) => (
               <IonCol size="6" key={index}>
-                <IonImg src={photo.webviewPath} />
+                <IonImg onClick={() => setPhotoToDelete(photo)} 
+                  src={photo.webviewPath} />
               </IonCol>
             ))}
           </IonRow>
       </IonGrid>
-        <IonFab vertical="bottom" horizontal="center" slot="fixed">
-            <IonFabButton onClick={() => takePhoto()}>
-              <IonIcon icon={camera}></IonIcon>
-            </IonFabButton>
-        </IonFab>
+      <IonFab vertical="bottom" horizontal="center" slot="fixed">
+        <IonFabButton onClick={() => takePhoto()}>
+          <IonIcon icon={camera}></IonIcon>
+        </IonFabButton>
+      </IonFab>
+      <IonActionSheet
+        isOpen={!!photoToDelete}
+        buttons={[{
+          text: 'Delete',
+          role: 'destructive',
+          icon: trash,
+          handler: () => {
+            if (photoToDelete) {
+              deletePhoto(photoToDelete);
+              setPhotoToDelete(undefined);
+            }
+          }
+        }, {
+          text: 'Cancel',
+          icon: close,
+          role: 'cancel'
+        }]}
+        onDidDismiss={() => setPhotoToDelete(undefined)}
+      />        
       </IonContent>
     </IonPage>
   );
